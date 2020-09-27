287d65392a3a41f668ae3ad5eb058da937673265
leewt95 <leewt.15@gmail.com>
2020-09-28 02:42:56 +0800

    [] Added axios and basic API logic

---
+++ components/RecipeOfTheDay.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import { Button, View, Text, Image } from 'react-native';
+
+const RecipeOfTheDay = (recipe) => {
+  return <View></View>;
+};
+
+export default RecipeOfTheDay;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import { Button, View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Button, View, Text, Image } from 'react-native';
+import TheMealDb from '../api/TheMealDb';
 
 const HomeScreen = ({ navigation }) => {
+  const [recipe, setRecipe] = useState([]);
+
+  const getRecipeOfTheDay = async () => {
+    try {
+      const response = await TheMealDb.get('/random.php');
+      setRecipe(response.data.meals[0]);
+    } catch (e) {
+      alert(e);
+    }
+  };
+
+  useEffect(() => {
+    getRecipeOfTheDay();
+  }, []);
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Welcome')}
+      <Image
+        source={{ uri: recipe.strMealThumb }}
+        style={{ width: 150, height: 150 }}
       />
+      <Text>{recipe.idMeal}</Text>
+      <Button title="Go to Details" onPress={() => getRecipeOfTheDay()} />
     </View>
   );
 };
