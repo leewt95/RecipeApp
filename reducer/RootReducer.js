ac8fb5b3000517a011b528b58c0ac9ef8aecc134
wentian.lee <wentian.lee@hexainitiative.com>
2020-10-08 17:20:45 +0800

    [] Combine two new reducer

--- reducer/RootReducer.js
+++ reducer/RootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers, createStore } from 'redux';
 import { RecipeAPIReducer } from './RecipeApiReducer';
 import { RecipeCategoriesReducer } from './RecipeCategoriesReducer';
+import { RecipeDatabaseReducer } from './RecipeDatabaseReducer';
 
 const rootReducer = combineReducers({
   recipeApiReducer: RecipeAPIReducer,
-  recipeCategoriesReducer: RecipeCategoriesReducer
+  recipeCategoriesReducer: RecipeCategoriesReducer,
+  recipeDatabaseReducer: RecipeDatabaseReducer
 });
 
 export const recipeStore = createStore(rootReducer);
