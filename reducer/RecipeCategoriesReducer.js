3d3d488af84d0298d28aeae9cf3cc9f5f61fe83b
wentian.lee <wentian.lee@hexainitiative.com>
2020-10-08 17:21:09 +0800

    [] Transfer functions from screen to reducer

--- reducer/RecipeCategoriesReducer.js
+++ reducer/RecipeCategoriesReducer.js
@@ -1,16 +1,17 @@
-import { READ_XML_CATEGORIES } from '../constants/Constants';
-import RecipeCategories from '../xml/RecipeCategories'
+import { READ_XML_CATEGORIES, UPDATE_SELECTED_CATEGORY } from '../constants/Constants';
+import RecipeCategories from '../xml/RecipeCategories';
 
 const initialState = {
   recipeCategories: [],
   firstCategoryValue: '',
+  selectedCategory: '',
 };
 
-export const readRecipeCategories = (dispatch) => {
+export const readRecipeCategories = async (dispatch) => {
   var strXml = require('react-native-xml2js').parseString;
   var xml = RecipeCategories;
   try {
-    strXml(xml, (err, result) => {
+    await strXml(xml, (err, result) => {
       dispatch({
         type: READ_XML_CATEGORIES,
         payload: result.RecipeCategories.Category,
@@ -21,6 +22,10 @@ export const readRecipeCategories = (dispatch) => {
   }
 };
 
+export const updateSelectedCategory = (value, dispatch) => {
+  dispatch({type: UPDATE_SELECTED_CATEGORY, payload: value})
+}
+
 export const RecipeCategoriesReducer = (state = initialState, action) => {
   switch (action.type) {
     case READ_XML_CATEGORIES:
@@ -28,6 +33,12 @@ export const RecipeCategoriesReducer = (state = initialState, action) => {
         ...state,
         recipeCategories: action.payload,
         firstCategoryValue: action.payload[0],
+        selectedCategory: action.payload[0]
+      };
+    case UPDATE_SELECTED_CATEGORY:
+      return {
+        ...state,
+        selectedCategory: action.payload,
       };
     default:
       return state;
