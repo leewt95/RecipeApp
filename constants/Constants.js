22c9a52564a0f0007ba8b92da8eb99fc2be99c3d
wentian.lee <wentian.lee@hexainitiative.com>
2020-10-13 12:36:49 +0800

    [] Updare Constants

--- constants/Constants.js
+++ constants/Constants.js
@@ -18,6 +18,8 @@ export const NAVIGATION_STACK = {
 export const DATABASE_RECIPE = 'Recipe';
 export const RECIPE_ID = 'idMeal';
 export const RECIPE_CATEGORY = 'strCategory';
+export const RECIPE_STR_INGREDIENT = 'strIngredient'
+export const RECIPE_STR_MEASURE = 'strMeasure'
 
 /*
  * Reducers
@@ -33,9 +35,9 @@ export const DATABASE_DELETE_RECIPE = 'DATABASE_DELETE_RECIPE';
 // RecipeFormsReducer
 export const SET_RECIPE_IMAGE = 'SET_RECIPE_IMAGE';
 export const SET_RECIPE_NAME = 'SET_RECIPE_NAME';
-export const SET_RECIPE_CATEGORY = 'SET_RECIPE_CATEGORY';
 export const SET_RECIPE_INSTRUCTION = 'SET_RECIPE_INSTRUCTION';
 export const ADD_RECIPE_INGREDIENT = 'ADD_RECIPE_INGREDIENT';
 export const REMOVE_RECIPE_INGREDIENT = 'REMOVE_RECIPE_INGREDIENT';
 export const ON_INGREDIENT_TEXT_CHANGE = 'ON_INGREDIENT_TEXT_CHANGE';
+export const SET_RECIPE_TO_EDIT = 'SET_RECIPE_TO_EDIT';
 export const RESET_RECIPE_FORMS = 'RESET_RECIPE_FORMS';
